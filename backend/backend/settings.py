775b81a0ab994c88a6bb5194cf591d67864bfa0d
Stoyan Kanev <74728512+stoyan-kanev@users.noreply.github.com>
2025-12-22 13:14:03 +0200

    Small changes to frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,22 +128,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DATABASE_NAME'),
-        "USER": os.getenv('DATABASE_USER'),
-        "PASSWORD": os.getenv('DATABASE_PASSWORD'),
-        "HOST": os.getenv('DATABASE_HOST'),
-        "PORT": os.getenv('DATABASE_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv('DATABASE_NAME'),
+#         "USER": os.getenv('DATABASE_USER'),
+#         "PASSWORD": os.getenv('DATABASE_PASSWORD'),
+#         "HOST": os.getenv('DATABASE_HOST'),
+#         "PORT": os.getenv('DATABASE_PORT'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
